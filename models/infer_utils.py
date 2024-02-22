91ae250cdb4fa7cbb5d737a08618a91aef02b7ca
Tri Nguyen <tnguy@mit.edu>
2024-02-22 10:07:39 -0500

    infer now normalize data properly

--- models/infer_utils.py
+++ models/infer_utils.py
@@ -2,22 +2,26 @@
 import torch
 from tqdm import tqdm
 
-
 @torch.no_grad()
-def infer(model, loader, to_numpy=True, verbose=True):
+def infer(model, loader, softmax=False, to_numpy=True, verbose=True):
     model.eval()
     y_pred, y_true = [], []
     if verbose:
         loop = tqdm(loader, desc="Inferencing")
     else:
         loop = loader
-    for x, y in loop:
-        x = x.to(model.device)
-        y = y.to(model.device)
+    for batch in loop:
+        batch_dict = model._prepare_training_batch(batch)
+        x = batch_dict['x']
+        y = batch_dict['y']
         y_pred.append(model(x).cpu())
         y_true.append(y.cpu())
     y_pred = torch.cat(y_pred, dim=0)
     y_true = torch.cat(y_true, dim=0)
+
+    if softmax:
+        y_pred = torch.nn.functional.softmax(y_pred, dim=1)
+
     if to_numpy:
         y_pred = y_pred.numpy()
         y_true = y_true.numpy()
